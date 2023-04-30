448a3763eb4f22338afcce7968abce82643e7526
artur-954 <125141206+artur-954@users.noreply.github.com>
2023-04-30 19:36:47 +0500

    Delete tinkoff2 directory

--- tinkoff2/GitHubClient.java
+++
@@ -1,4 +0,0 @@
-public interface GitHubClient {
-    @GetMapping("/repos/{owner}/{repo}/commits/{ref}")
-    Mono<List<CommitResponse>> fetchCommits(@PathVariable String owner, @PathVariable String repo, @PathVariable String ref);
-}
